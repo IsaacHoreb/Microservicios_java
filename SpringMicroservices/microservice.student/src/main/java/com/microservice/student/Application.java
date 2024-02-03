fef6b0fb167073d4c6673420ec80955b916cb019
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-02-03 00:14:06 -0600

    Configuracion del archivo .yml para Student

--- SpringMicroservices/microservice.student/src/main/java/com/microservice/student/Application.java
+++ SpringMicroservices/microservice.student/src/main/java/com/microservice/student/Application.java
@@ -2,12 +2,15 @@ package com.microservice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//Agregamos esta anotacion, no es obligatoria pero lo tenemos
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
