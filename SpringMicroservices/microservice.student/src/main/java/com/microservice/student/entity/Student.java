fe2b1ca32103b00b82244356d4cfea26374f14d6
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-02-05 20:52:21 -0600

    Creacion de la entidad student

---
+++ SpringMicroservices/microservice.student/src/main/java/com/microservice/student/entity/Student.java
@@ -0,0 +1,31 @@
+package com.microservice.student.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+}
