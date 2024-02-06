f893606388bedcf1ce5a4fe44923401287da2001
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-02-05 23:09:01 -0600

    Creacion de servicio a students

---
+++ SpringMicroservices/microservice.student/src/main/java/com/microservice/student/service/Impl/StudentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.microservice.student.service.Impl;
+
+import com.microservice.student.entity.Student;
+import com.microservice.student.persistences.StudentRepository;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements IStudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return studentRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void save(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> findByIdCourse(Long idCourse) {
+        return studentRepository.findAllStudent(idCourse);
+    }
+
+}
