3717a24e88c585ba9c6dd4d3f4bc1c551e598bdc
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-02-06 21:49:33 -0600

    Modificacion del cliente y configuracion por interfaz

---
+++ SpringMicroservices/microservice.course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -0,0 +1,16 @@
+package com.microservice.course.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+//Nombre del microservice a conectarse, puerto y endPont del controlador
+@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
+public interface StudentClient { //Modificacion del cliente
+
+    @GetMapping("/search-by-course/{idCourse}")
+    List<?> findAllStudentByCourse(@PathVariable("idCourse") Long idCourse); //Con esto le decimos que consumira este endPoint del controladr
+
+}
