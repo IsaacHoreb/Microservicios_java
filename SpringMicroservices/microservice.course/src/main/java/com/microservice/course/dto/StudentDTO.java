26a1d6b449456c9e748e3d6b7e8d2daa989e5ea1
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2024-02-06 22:02:25 -0600

    Creacion del StudentDTO para usarlo en el client de Course

---
+++ SpringMicroservices/microservice.course/src/main/java/com/microservice/course/dto/StudentDTO.java
@@ -0,0 +1,19 @@
+package com.microservice.course.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StudentDTO {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private Long courseId;
+
+}
